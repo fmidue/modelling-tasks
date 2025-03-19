c39e9a0a58036e63edea55e2108e0b2e3af70c58
Luca E. <eichhofer02@web.de>
2025-03-19 23:42:50 +0100

    removed brackets

--- src/Modelling/PetriNet/Types.hs
+++ src/Modelling/PetriNet/Types.hs
@@ -1094,7 +1094,7 @@ checkGraphLayouts useDifferent wrongInstances gc
   = Just "At least one graph layout needs to be provided."
   | useDifferent && length (graphLayouts gc) <= wrongInstances
   = Just "The parameter 'graphLayout' has to contain more entries than the number of 'wrongInstances' if 'useDifferentGraphLayouts' is set."
-  | Patchwork `elem` (graphLayouts gc)
+  | Patchwork `elem` graphLayouts gc
   = Just "Do not use 'Patchwork' as a GraphViz Renderer as it does not work properly."
   | otherwise
   = Nothing
