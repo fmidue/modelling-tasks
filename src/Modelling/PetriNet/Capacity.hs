bc89a249cf4f918aaa01a00d5fcfe6fd69c5cd5a
Luca E. <eichhofer02@web.de>
2025-04-06 18:00:53 +0200

    implemented capacityConfig parameters into alloy-code

--- src/Modelling/PetriNet/Capacity.hs
+++ src/Modelling/PetriNet/Capacity.hs
@@ -391,6 +391,9 @@ pred #{capacityPredicateName}[#{activated} : set Transitions] {
       no p.complement.flow[t] and no t.flow[p.complement]
   all p : placesWithCapacity, w : p.flow[Transitions] + Transitions.flow[p] | p.capacity >= w
 
+  #{minNewArrowsWithComplementConstraints minNewArrowsWithComplement}
+  #{oneMinCapacityConstraints oneMinCapacity}
+
 }
 
 run #{capacityPredicateName} for exactly #{places basicC} givenPlaces, exactly #{places basicC} addedPlaces,
@@ -398,10 +401,20 @@ exactly #{transitions basicC} Transitions, #{petriScopeBitWidth (basicCBitWidth 
 |]
   where
     activated = skolemName
-    defaultConstraintsAtLeastZero :: String -> BasicConfig -> String
-    defaultConstraintsAtLeastZero activated basicC =
-      enforceConstraints True Nothing activated (basicC { atLeastActive = 0 }) ++
-      concat [ [i|\##{activated} >= #{atLeastActive basicC}|] | atLeastActive basicC > 0]
+    minNewArrowsWithComplementConstraints :: Maybe Int -> String
+    minNewArrowsWithComplementConstraints minNewArrows =
+      case minNewArrows of
+        Just minNew ->
+          "let totalFlow = " ++
+          "#({p: placesWithCapacity | some p.complement.flow[Transitions]}.complement.flow[Transitions]) + " ++
+          "#(Transitions.flow[{p: placesWithCapacity | some Transitions.flow[p.complement]}.complement]) | " ++
+          "totalFlow >= " ++ show minNew
+        Nothing -> ""
+    oneMinCapacityConstraints :: Maybe Int -> String
+    oneMinCapacityConstraints oneMinCap =
+      case oneMinCap of
+        Just oneMin -> [i|some p : placesWithCapacity | p.capacity >= #{oneMin}|]
+        Nothing -> ""
 
 capacityPredicateName :: String
 capacityPredicateName = "showCapacity"
