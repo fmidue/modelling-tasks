fccc61081ef8a633b65eff613a2f922f6a051a32
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2025-04-20 14:05:00 +0200

    make test compile again

--- test/Modelling/PetriNet/DiagramSpec.hs
+++ test/Modelling/PetriNet/DiagramSpec.hs
@@ -27,7 +27,7 @@ spec =
       do
         (inst:_) <- getInstances (Just 1)
            (petriNetRnd defaultBasicConfig defaultAdvConfig)
-        pl <- parseNet "flow" "tokens" Nothing inst
+        pl <- parseNet "flow" "tokens" inst
         dia <- drawNet show (pl :: SimplePetriLike Object) DrawSettings {
           withPlaceNames = True,
           withSvgHighlighting = True,
