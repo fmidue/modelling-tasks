dfe15c46c33fcf6cb04528338b861181f8835d2e
Luca E. <eichhofer02@web.de>
2025-04-21 00:02:09 +0200

    added testing for capacity

---
+++ test/Modelling/PetriNet/CapacitySpec.hs
@@ -0,0 +1,109 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
+
+module Modelling.PetriNet.CapacitySpec where
+
+import qualified Modelling.PetriNet.Types         as Find (
+  CapacityConfig (alloyConfig),
+  )
+
+import Modelling.PetriNet.Capacity (
+  checkCapacityConfigs,
+  checkCapacityConfig,
+  combinedCapacityInstance,
+  petriNetFindCapacity,
+  )
+import Modelling.PetriNet.Diagram (
+  getDefaultNet,
+  getNet,
+  )
+import Modelling.PetriNet.Parser (
+  parseChange,
+  )
+import Modelling.PetriNet.Types (
+  AdvConfig (AdvConfig),
+  BasicConfig (..),
+  CapacityConfig (CapacityConfig),
+  CapacityNode (..),
+  PetriLike (..),
+  PetriChangeList (..),
+  SimplePetriLike,
+  defaultCapacityConfig,
+  toChangeList,
+  )
+
+import Modelling.PetriNet.TestCommon (
+  alloyTestConfig,
+  checkConfigs,
+  defaultConfigTaskGeneration,
+  firstInstanceConfig,
+  testTaskGeneration,
+  validAdvConfigs,
+  validConfigsForPick,
+  validGraphConfig,
+  )
+import Settings                         (configDepth)
+import Data.Maybe                       (isNothing)
+
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "defaultCapacityConfig" $
+    checkConfigs checkCapacityConfigs [defaultCapacityConfig]
+  describe "validFindCapacityConfigs" $
+    checkConfigs checkCapacityConfigs findConfigs'
+  describe "combinedCapacity" $ do
+    defaultConfigTaskGeneration
+      (combinedCapacityInstance defaultCapacityConfig {
+          Find.alloyConfig = firstInstanceConfig
+          } 0)
+      0
+      $ checkCapacityInstance @(SimplePetriLike _)
+    testCapacityConfig findConfigs
+  where
+    findConfigs' = validFindCapacityConfigs
+      validFinds
+      (AdvConfig Nothing Nothing Nothing)
+    findConfigs = validAdvConfigs >>= validFindCapacityConfigs validFinds
+    validFinds = validConfigsForPick 0 configDepth
+
+checkCapacityInstance :: (PetriLike CapacityNode String, a, PetriChangeList String) -> Bool
+checkCapacityInstance (_, _, change) = isValidCapacity change
+
+testCapacityConfig :: [CapacityConfig] -> Spec
+testCapacityConfig = testTaskGeneration
+  petriNetFindCapacity
+  (\inst -> do
+    first <- getDefaultNet inst
+    (second, third) <- getNet (fmap toChangeList . parseChange) inst
+    return (first, second, third)
+  )
+  $ checkCapacityInstance @(SimplePetriLike _)
+
+validFindCapacityConfigs :: [(BasicConfig, _)] -> AdvConfig -> [CapacityConfig]
+validFindCapacityConfigs cs advancedConfig = do
+  (bc, _) <- cs
+  (maxCapacity, newArrows, oneMin, distractors, atMost) <- validCapacityConfig bc
+  return $ CapacityConfig bc advancedConfig maxCapacity newArrows oneMin distractors atMost validGraphConfig False alloyTestConfig
+
+validCapacityConfig :: BasicConfig -> [(Int, (Int, Int), Int, (Int, Int), Maybe Int)]
+validCapacityConfig bc@BasicConfig{ places, transitions, maxFlowPerEdge, maxTokensPerPlace, atLeastActive } =
+  filter (\(maxCap, arrows, oneMinCap, distract, most) -> isNothing (checkCapacityConfig bc maxCap arrows oneMinCap distract most)) $ do
+    maxCapacity <- [max maxFlowPerEdge maxTokensPerPlace .. 5]
+    newArrows <- [(a, b) | a <- [places .. 2 * transitions * places], b <- [a .. 2 * transitions * places]]
+    oneMin <- [1 .. maxCapacity]
+    distractors <- [(x, y) | x <- [0 .. transitions], y <- [transitions .. transitions - atLeastActive], x <= y]
+    atMost <- Nothing : [Just n | n <- [0 .. transitions]]
+    return (maxCapacity, newArrows, oneMin, distractors, atMost)
+
+isValidCapacity :: PetriChangeList String -> Bool
+isValidCapacity c@(ChangeList {tokenChanges = ts, flowChanges = fs})
+  | any (\(_, token) -> token < 0) ts                 = error $ show c
+  | any (\(source, target, _) -> source == target) fs = error $ show c
+  | any (\(_, _, flow) -> flow <= 0) fs               = error $ show c
+  | otherwise                                         = True
