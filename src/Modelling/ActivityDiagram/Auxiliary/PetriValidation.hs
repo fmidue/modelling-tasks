3e035fb0ed7e3e64f395c6db118761017d952954
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2025-09-16 20:22:41 +0200

    restore change after merge conflict resolution
    Added validation for negative values in 'countOfPetriNodesBounds'.

--- src/Modelling/ActivityDiagram/Auxiliary/PetriValidation.hs
+++ src/Modelling/ActivityDiagram/Auxiliary/PetriValidation.hs
@@ -27,6 +27,10 @@ validatePetriConfig adConfig maxInstances petriLayout auxiliaryPetriNodeAbsent p
   = Just "There is at most one 'activityFinalNode' allowed."
   | Config.activityFinalNodes adConfig >= 1 && Config.flowFinalNodes adConfig >= 1
   = Just "There is no 'flowFinalNode' allowed if there is an 'activityFinalNode'."
+  | fst countOfPetriNodesBounds < 0
+  = Just "'countOfPetriNodesBounds' must not contain negative values"
+  | Just high <- snd countOfPetriNodesBounds, fst countOfPetriNodesBounds > high
+  = Just "the second value of 'countOfPetriNodesBounds' must not be smaller than its first value"
   | isJust maxInstances && fromJust maxInstances < 1
     = Just "The parameter 'maxInstances' must either be set to a positive value or to Nothing"
   | auxiliaryPetriNodeAbsent == Just True && Config.cycles adConfig > 0
