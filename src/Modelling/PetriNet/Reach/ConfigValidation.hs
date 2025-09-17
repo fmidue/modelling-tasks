377c39f66118c3e512fef4e3cbed3ffd8bd30303
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2025-09-17 23:55:27 +0200

    Remove redundant minTransitionLength check for rejectLongerThan

--- src/Modelling/PetriNet/Reach/ConfigValidation.hs
+++ src/Modelling/PetriNet/Reach/ConfigValidation.hs
@@ -47,8 +47,6 @@ checkRejectLongerThanConsistency rejectLongerThan minTransitionLength maxTransit
   case rejectLongerThan of
     Just rejectLength
       | rejectLength <= 0 -> Just "rejectLongerThan must be positive when specified"
-      | rejectLength < minTransitionLength -> Just $
-        "rejectLongerThan (" ++ show rejectLength ++ ") cannot be less than minTransitionLength (" ++ show minTransitionLength ++ ")"
       | rejectLength < maxTransitionLength -> Just $
         "rejectLongerThan (" ++ show rejectLength ++ ") cannot be less than maxTransitionLength (" ++ show maxTransitionLength ++ ")"
       | rejectLength == maxTransitionLength && showLengthHint -> Just "showLengthHint cannot be True when rejectLongerThan equals maxTransitionLength"
