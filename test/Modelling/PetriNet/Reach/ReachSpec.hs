fc777c7cba0020dee2f4b3402e996e0d16daab62
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2025-09-18 14:21:10 +0200

    Remove boring ReachConfig acceptance tests
    Removed tests for configurations with showTargetNet and showPlaceNamesInNet.

--- test/Modelling/PetriNet/Reach/ReachSpec.hs
+++ test/Modelling/PetriNet/Reach/ReachSpec.hs
@@ -173,20 +173,6 @@ spec = do
             }
       checkReachConfig config `shouldSatisfy` isJust
 
-    it "accepts configuration with showTargetNet = True and showPlaceNamesInNet = False" $ do
-      let config = defaultReachConfig {
-            showTargetNet = True,
-            showPlaceNamesInNet = False
-            }
-      checkReachConfig config `shouldBe` Nothing
-
-    it "accepts configuration with showTargetNet = False and showPlaceNamesInNet = True" $ do
-      let config = defaultReachConfig {
-            showTargetNet = False,
-            showPlaceNamesInNet = True
-            }
-      checkReachConfig config `shouldBe` Nothing
-
     it "rejects configuration with both showTargetNet = False and showPlaceNamesInNet = False" $ do
       let config = defaultReachConfig {
             showTargetNet = False,
