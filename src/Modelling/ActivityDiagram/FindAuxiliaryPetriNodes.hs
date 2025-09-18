b45ea934c9086f296fa8bedb0bde10789953b5fc
nimec01 <24428341+nimec01@users.noreply.github.com>
2025-09-18 13:56:55 +0200

    Revert "add syntax checks for FindAuxiliaryPetriNodes"
    This reverts commit d822e3a0635754bdee4800e06a5f4d67259de436.

--- src/Modelling/ActivityDiagram/FindAuxiliaryPetriNodes.hs
+++ src/Modelling/ActivityDiagram/FindAuxiliaryPetriNodes.hs
@@ -20,7 +20,6 @@ module Modelling.ActivityDiagram.FindAuxiliaryPetriNodes (
   findAuxiliaryPetriNodesEvaluation,
   findAuxiliaryPetriNodesInitial,
   findAuxiliaryPetriNodesSolution,
-  findAuxiliaryPetriNodesSyntax,
   findAuxiliaryPetriNodesTask,
 ) where
 
@@ -249,16 +248,6 @@ findAuxiliaryPetriNodesInitial = FindAuxiliaryPetriNodesSolution {
   countOfAuxiliaryPlaces = 2,
   countOfAuxiliaryTransitions = 3
 }
-
-findAuxiliaryPetriNodesSyntax
-  :: OutputCapable m
-  => FindAuxiliaryPetriNodesInstance
-  -> FindAuxiliaryPetriNodesSolution
-  -> LangM m
-findAuxiliaryPetriNodesSyntax _ FindAuxiliaryPetriNodesSolution {..} = addPretext $ do
-  assertion (countOfAuxiliaryPlaces + countOfAuxiliaryTransitions < countOfPetriNodes) $ translate $ do
-    english "Petri net does not only consist of auxiliary places and transitions?"
-    german "Petrinetz besteht nicht nur aus Hilfsstellen und -transitionen?"
 
 findAuxiliaryPetriNodesEvaluation
   :: OutputCapable m
