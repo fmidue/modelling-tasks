b7c724444a9a4d87d65f0348308cc399084145f6
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2025-10-14 13:22:49 +0200

    Reorder object names warning in EnterAS.hs

--- src/Modelling/ActivityDiagram/EnterAS.hs
+++ src/Modelling/ActivityDiagram/EnterAS.hs
@@ -262,13 +262,6 @@ enterASEvaluation task sub = do
     english "The submitted action sequence is correct?"
     german "Die eingereichte Aktionsfolge ist korrekt?"
 
-  unless (null objectNamesInSubmission) $ do
-    translate $ do
-      english "The following referenced nodes are object nodes and thus not actions:"
-      german "Die folgenden referenzierten Knoten sind Objektknoten und damit keine Aktionen:"
-    code $ intercalate ", " objectNamesInSubmission
-    pure ()
-
   when (null objectNamesInSubmission && not reachesZeroState) $ do
     let finalNodeReached = any (any (\(_, path) -> any isFinalPetriNode path)) levels
     when finalNodeReached $ do
@@ -286,6 +279,13 @@ enterASEvaluation task sub = do
           A complete solution must terminate all flows present in the diagram.
           |]
       pure ()
+
+  unless (null objectNamesInSubmission) $ do
+    translate $ do
+      english "The following referenced nodes are object nodes and thus not actions:"
+      german "Die folgenden referenzierten Knoten sind Objektknoten und damit keine Aktionen:"
+    code $ intercalate ", " objectNamesInSubmission
+    pure ()
 
   printSolutionAndAssert IndefiniteArticle maybeSolutionString points
 
