0ef2653d0be75ff51c63a63d4f97092cfba4278b
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2025-10-17 07:41:38 +0200

    Refactor sequence generation for zeroState

--- src/Modelling/ActivityDiagram/ActionSequences.hs
+++ src/Modelling/ActivityDiagram/ActionSequences.hs
@@ -127,7 +127,7 @@ generateSequencesWithLevels levelsFunction petriLike =
   let petri = fromPetriLike petriLike
       zeroState = State $ M.map (const 0) $ unState $ start petri
       allLevels = levelsFunction petri
-  in map (reverse . snd) $ concatMap (filter ((== zeroState) . fst)) allLevels
+  in [reverse p | level <- allLevels, (s, p) <- level, s == zeroState]
 
 -- | Variant of levels' that manages visited states per path rather than globally
 -- This allows exploring cycles while preventing infinite loops within each path
