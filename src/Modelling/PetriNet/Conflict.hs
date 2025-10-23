4071120fd17cb1eadde3809096cc4119826edcfd
Ching Pei Yang <badnam3o.0@gmail.com>
2025-10-23 13:57:01 +0200

    remove extraneous paragraph

--- src/Modelling/PetriNet/Conflict.hs
+++ src/Modelling/PetriNet/Conflict.hs
@@ -355,7 +355,7 @@ pickConflictTask showInputHelp path task = do
       die unter der Startmarkierung in Konflikt stehen?
       |]
   images show snd $=<< renderPick path task
-  when showInputHelp $ paragraph $ do
+  when showInputHelp $ do
     paragraph $ translate $ do
       english [iii|
         State your answer by giving the number of the Petri net
