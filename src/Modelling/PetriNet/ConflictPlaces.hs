8ddb82fb31fd00617bb5702a0c67e9c02c42ecd2
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2025-10-26 08:21:50 +0100

    rephrasing

--- src/Modelling/PetriNet/ConflictPlaces.hs
+++ src/Modelling/PetriNet/ConflictPlaces.hs
@@ -134,10 +134,8 @@ findConflictPlacesTask showInputHelp path task = do
     german "Welches Paar von Transitionen steht in Konflikt, und wegen welcher konfliktverursachenden Stelle(n), unter der Startmarkierung?"
   if not showInputHelp then
     paragraph $ translate $ do
-     english [i|A place causes conflict if it is a common precondition of both conflicting transitions, and does not
-have enough tokens for firing both transitions at the same time.|]
-     german [i|Eine Stelle verursacht einen Konflikt, wenn sie eine gemeinsame Vorbedingungen für beide in Konflikt
-stehenden Transitionen ist und sie nicht ausreichend Marken hat, um die Transitionen gleichzeitig zu feuern.|]
+     english [i|You have to indicate all the places that induce the conflict, i.e., all those common places within the preconditions which each separately do not have enough tokens for firing the two transitions at the same time.|]
+     german [i|Sie müssen alle Stellen angeben, die den Konflikt verursachen, also all jene gemeinsamen Stellen in den Vorbedingungen, die jeweils einzeln nicht ausreichend Marken zum gleichzeitigen Feuern der beiden haben.|]
   else paragraph $ do
     translate $ do
       english "State your answer by indicating a pair of conflicting transitions and a list of all the places that induce the conflict. "
