71da201dcb1c2601594fec750663a203f3bc9e1a
Marcellus Siegburg <531939+marcellussiegburg@users.noreply.github.com>
2025-10-27 19:34:49 +0100

    Clean-up testing

--- test/Modelling/PetriNet/Reach/FilterSpec.hs
+++ test/Modelling/PetriNet/Reach/FilterSpec.hs
@@ -1,95 +1,101 @@
+{-# LANGUAGE TypeApplications #-}
 module Modelling.PetriNet.Reach.FilterSpec where
 
 import Modelling.PetriNet.Reach.Filter
 import Modelling.PetriNet.Reach.Type (Transition(..))
 
+import Data.List                        (zipWith4, zipWith5, zipWith6, zipWith7)
+import Data.List.Extra                  (nubOrd)
 import Test.Hspec
+import Test.QuickCheck (
+  Arbitrary (arbitrary),
+  Gen,
+  Testable (property),
+  (==>),
+  chooseInt,
+  forAll,
+  suchThat,
+  vectorOf,
+  )
+
+genNub :: (Arbitrary a, Ord a) => Gen [a]
+genNub = nubOrd <$> arbitrary
+
+genNubSized :: (Arbitrary a, Ord a) => Int -> Gen [a]
+genNubSized n = vectorOf n arbitrary `suchThat` noDuplicates
+  where
+    noDuplicates xs = length xs == length (nubOrd xs)
+
+zipN :: Int -> [a] -> [a]
+zipN n xs = concat $ case n of
+  2 -> zipWith (\as bs -> [as, bs]) xs xs
+  3 -> zipWith3 (\as bs cs -> [as, bs, cs]) xs xs xs
+  4 -> zipWith4 (\as bs cs ds -> [as, bs, cs, ds]) xs xs xs xs
+  5 -> zipWith5 (\as bs cs ds es -> [as, bs, cs, ds, es]) xs xs xs xs xs
+  6 -> zipWith6
+    (\as bs cs ds es fs -> [as, bs, cs, ds, es, fs])
+    xs xs xs xs xs xs
+  7 -> zipWith7
+    (\as bs cs ds es fs gs -> [as, bs, cs, ds, es, fs, gs])
+    xs xs xs xs xs xs xs
+  _ -> error $ "zipN is for parameter " ++ show n ++ " not defined"
 
 spec :: Spec
 spec = do
   describe "isCyclicPattern" $ do
-    it "detects simple cyclic patterns" $ do
-      isCyclicPattern 4 ([Transition 1, Transition 2, Transition 3, Transition 4] ++ [Transition 1, Transition 2, Transition 3, Transition 4]) `shouldBe` True
+    it "detects cyclic patterns" $
+      forAll (chooseInt (2, 9)) $ \m ->
+        forAll (chooseInt (m, 9)) $ \n ->
+          forAll (genNubSized m) $
+            isCyclicPattern @Int n . concat . replicate 2
 
-    it "detects cyclic patterns with multiple repetitions" $ do
-      isCyclicPattern 4 ([Transition 1, Transition 2] ++ [Transition 1, Transition 2] ++ [Transition 1, Transition 2] ++ [Transition 1, Transition 2]) `shouldBe` True
+    it "does not detect too large cyclic patterns" $
+      forAll (chooseInt (3, 9)) $ \m ->
+        forAll (chooseInt (2, m - 1)) $ \n ->
+          forAll (genNubSized m) $
+            not . isCyclicPattern @Int n . concat . replicate 2
 
-    it "does not detect partial cycles" $ do
-      isCyclicPattern 4 ([Transition 1, Transition 2, Transition 3, Transition 4] ++ [Transition 1, Transition 2]) `shouldBe` False
-
-    it "does not detect single cycles" $ do
-      isCyclicPattern 4 [Transition 1, Transition 2, Transition 3, Transition 4] `shouldBe` False
-
-    it "handles empty lists" $ do
-      isCyclicPattern 4 ([] :: [Transition]) `shouldBe` False
-
-    it "handles short lists" $ do
-      isCyclicPattern 4 [Transition 1, Transition 2] `shouldBe` False
+    it "does only detect complete cyclic patterns" $
+      forAll (chooseInt (0, 9)) $ \m ->
+        forAll (chooseInt (0, 9)) $ \n ->
+          forAll (genNubSized m) $ \xs ->
+            forAll (genNubSized n) $ \ys ->
+              xs /= ys ==> not $ isCyclicPattern @Int (m + n) $ xs ++ ys
 
   describe "hasRepetitiveSubsequence" $ do
-    it "detects repetitive prefixes" $ do
-      hasRepetitiveSubsequence 3 [Transition 1, Transition 1, Transition 1, Transition 2] `shouldBe` True
-      hasRepetitiveSubsequence 4 [Transition 4, Transition 4, Transition 4, Transition 4] `shouldBe` True
+    it "detects repetitive prefixes" $
+      forAll (chooseInt (2, 9)) $ \m ->
+        forAll (chooseInt (m, 9)) $ \n ->
+          forAll (genNubSized m) $ \xs ->
+            hasRepetitiveSubsequence @Int m $ replicate (n - 1) (head xs) ++ xs
 
-    it "detects repetitive suffixes" $ do
-      hasRepetitiveSubsequence 3 [Transition 1, Transition 2, Transition 3, Transition 3, Transition 3] `shouldBe` True
+    it "detects repetitive suffixes" $
+      forAll (chooseInt (2, 9)) $ \m ->
+        forAll (chooseInt (m, 9)) $ \n ->
+          forAll (genNubSized m) $ \xs ->
+            hasRepetitiveSubsequence @Int m $ xs ++ replicate (n - 1) (last xs)
 
-    it "does not detect short repetitions" $ do
-      hasRepetitiveSubsequence 3 [Transition 1, Transition 1, Transition 2] `shouldBe` False
-
-    it "handles minimum length requirement" $ do
-      hasRepetitiveSubsequence 5 [Transition 1, Transition 1, Transition 1] `shouldBe` False
-      hasRepetitiveSubsequence 2 [Transition 1, Transition 1] `shouldBe` True
+    it "does not detect too short repetitions" $ do
+      forAll (chooseInt (2, 9)) $ \m ->
+        forAll (chooseInt (0, m - 1)) $ \n ->
+          forAll (genNubSized m) $ \xs ->
+            not $ hasRepetitiveSubsequence @Int m $
+              replicate (n - 1) (head xs) ++ xs ++ replicate (n - 1) (last xs)
 
   describe "hasGroupedRepeats" $ do
     it "detects grouped repeats pattern" $ do
-      hasGroupedRepeats [Transition 1, Transition 1, Transition 2, Transition 2,
-                         Transition 3, Transition 3, Transition 4, Transition 4] `shouldBe` True
+      forAll (chooseInt (2, 9)) $ \m ->
+        forAll (chooseInt (2, 7)) $ \n ->
+          forAll (genNubSized m) $ \xs ->
+            length xs >= 2 ==> hasGroupedRepeats @Int (zipN n xs)
 
-    it "detects grouped repeats with different group sizes" $ do
-      hasGroupedRepeats [Transition 1, Transition 1, Transition 1,
-                         Transition 2, Transition 2, Transition 2] `shouldBe` True
-
-    it "does not detect single elements" $ do
-      hasGroupedRepeats [Transition 1, Transition 2, Transition 3, Transition 4] `shouldBe` False
-
-    it "does not detect mixed patterns" $ do
-      hasGroupedRepeats [Transition 1, Transition 1, Transition 2,
-                         Transition 3, Transition 3, Transition 3] `shouldBe` False
-
-    it "handles empty and short lists" $ do
-      hasGroupedRepeats ([] :: [Transition]) `shouldBe` False
-      hasGroupedRepeats [Transition 1, Transition 2] `shouldBe` False
-
-  describe "isTrivialSequence" $ do
-    it "detects the Spaceballs PIN pattern" $ do
-      let spaceballsPattern = [Transition 1, Transition 2, Transition 3, Transition 4,
-                               Transition 1, Transition 2, Transition 3, Transition 4]
-      isTrivialSequence defaultFilterConfig spaceballsPattern `shouldBe` True
-
-    it "detects repetitive patterns" $ do
-      let repetitivePattern = [Transition 4, Transition 4, Transition 4, Transition 4]
-      isTrivialSequence defaultFilterConfig repetitivePattern `shouldBe` True
-
-    it "detects grouped repeats" $ do
-      let groupedPattern = [Transition 1, Transition 1, Transition 2, Transition 2,
-                            Transition 3, Transition 3, Transition 4, Transition 4]
-      isTrivialSequence defaultFilterConfig groupedPattern `shouldBe` True
-
-    it "does not flag legitimate sequences" $ do
-      let legitimatePattern = [Transition 1, Transition 3, Transition 2, Transition 2,
-                               Transition 4, Transition 3, Transition 1, Transition 1]
-      isTrivialSequence defaultFilterConfig legitimatePattern `shouldBe` False
-
-    it "can be used to filter out trivial solutions while keeping legitimate ones" $ do
-      let trivial1 = [Transition 4, Transition 2, Transition 3, Transition 1,
-                      Transition 4, Transition 2, Transition 3, Transition 1]
-      let trivial2 = [Transition 2, Transition 2, Transition 2, Transition 2]
-      let legitimate = [Transition 1, Transition 3, Transition 2, Transition 4,
-                        Transition 1, Transition 3, Transition 2, Transition 1]
-      let solutions = [trivial1, legitimate, trivial2]
-      filter (not . isTrivialSequence defaultFilterConfig) solutions
-        `shouldBe` [legitimate]
+    it "does not detect intersepted grouped repeats" $ do
+      forAll (chooseInt (2, 9)) $ \m ->
+        forAll (chooseInt (2, 7)) $ \n ->
+          forAll (chooseInt (0, m * n)) $ \i ->
+            forAll (genNubSized m) $ \xs ->
+              property $ \x -> not $ hasGroupedRepeats @Int
+                (let (front, end) = splitAt i (zipN n xs) in front ++ x : end)
 
   describe "configuration" $ do
     it "respects filter configuration settings" $ do
@@ -97,18 +103,3 @@ spec = do
       let configNoCyclic = defaultFilterConfig { filterCyclicPatterns = False }
       isTrivialSequence configNoCyclic cyclicPattern `shouldBe` False
       isTrivialSequence defaultFilterConfig cyclicPattern `shouldBe` True
-
-  describe "real-world patterns" $ do
-    it "correctly identifies the Spaceballs PIN pattern" $ do
-      let spaceballsPin = [Transition 1, Transition 2, Transition 3, Transition 4,
-                           Transition 1, Transition 2, Transition 3, Transition 4]
-      isTrivialSequence defaultFilterConfig spaceballsPin `shouldBe` True
-
-    it "correctly identifies simple repetitive patterns" $ do
-      let simpleRepetitive = [Transition 4, Transition 4, Transition 4, Transition 4]
-      isTrivialSequence defaultFilterConfig simpleRepetitive `shouldBe` True
-
-    it "allows legitimate complex patterns" $ do
-      let legitimatePattern = [Transition 1, Transition 3, Transition 2, Transition 4,
-                               Transition 3, Transition 1, Transition 4, Transition 2]
-      isTrivialSequence defaultFilterConfig legitimatePattern `shouldBe` False
