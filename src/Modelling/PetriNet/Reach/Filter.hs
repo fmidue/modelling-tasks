f3a5e7731451484b0a95e929290d50043cadd7e0
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2025-12-14 19:50:19 +0100

    Reduce max number of solutions to 50

--- src/Modelling/PetriNet/Reach/Filter.hs
+++ src/Modelling/PetriNet/Reach/Filter.hs
@@ -87,7 +87,7 @@ defaultFilterConfig = FilterConfig {
   minRepetitiveLength = Just 3,
   minSpaceballsLength = Just 4,
   maxCycleLength = Just 4,
-  maxNumberOfSolutions = Just 100,
+  maxNumberOfSolutions = Just 50,
   minTransitionCoverage = Just (4 % 5)
   }
 
