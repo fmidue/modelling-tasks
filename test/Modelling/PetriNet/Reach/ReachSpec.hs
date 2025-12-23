0fd7bdc1ebeddf6a2ca11134a033a4dea5eac54f
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2025-12-23 15:07:38 +0100

    undefined case in shortestSolutions

--- test/Modelling/PetriNet/Reach/ReachSpec.hs
+++ test/Modelling/PetriNet/Reach/ReachSpec.hs
@@ -70,7 +70,7 @@ spec = do
               minTransitionLength = 8
               }
         inst <- generateReach config seed
-        let allSolutions = either id id (shortestSolutions inst)
+        let allSolutions = either undefined id (shortestSolutions inst)
             availableTransitions = transitions $ petriNet $ netGoal inst
         allSolutions `shouldSatisfy` not . areSolutionsTrivial (filterConfig config) availableTransitions
 
