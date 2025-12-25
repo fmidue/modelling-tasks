56b99aaf8d32778c5fb6b50ab4b66ed7f3ee970e
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2025-12-25 07:47:11 +0200

    Comment out needsTuning for deadlock spec test

--- test/Modelling/PetriNet/Reach/DeadlockSpec.hs
+++ test/Modelling/PetriNet/Reach/DeadlockSpec.hs
@@ -50,8 +50,8 @@ spec = do
         net `shouldSatisfy`
           hasMinTransitionLength (null . successors net) ts minL
 
-    needsTuning $
-      it "generates non-trivial solutions when filtering is enabled" $
+    -- needsTuning $
+    it "generates non-trivial solutions when filtering is enabled" $
         quickCheckWith stdArgs {maxSuccess = 15} $ property $ \seed -> do
           let config = defaultDeadlockConfig
           deadlockInstance <- generateDeadlock config seed
