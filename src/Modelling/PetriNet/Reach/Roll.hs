869f02d6066688ccadcc46cab7ea85c9a869efbe
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2026-01-18 14:42:30 +0100

    move function

--- src/Modelling/PetriNet/Reach/Roll.hs
+++ src/Modelling/PetriNet/Reach/Roll.hs
@@ -172,40 +172,6 @@ generateFusableConnections allPlaces allTransitions numConsumingFusable numProdu
          , transitionProducingBimap  -- bimap from fusable producing-transitions to their places
          )
 
--- | Apply filtering checks to a net based on arrow density constraints and transition behavior
-applyNetFiltering
-  :: (Ord s, Ord t)
-  => ArrowDensityConstraints
-  -> TransitionBehaviorConstraints
-  -> Net s t
-  -> Maybe (Net s t)
-applyNetFiltering ArrowDensityConstraints{..} transitionBehaviorConstraints n = do
-  -- Filter out nets with isolated nodes
-  guard $ not $ hasIsolatedNodes n
-  -- Filter out nets that don't satisfy transition behavior constraints
-  guard $ satisfiesTransitionBehaviorConstraints n transitionBehaviorConstraints
-  -- Filter out nets that don't satisfy arrow density constraints beyond incomingArrowsPerTransition and outgoingArrowsPerTransition
-  let allTransToPlaces = concatMap (\(_, _, post) -> post) (connections n)
-  let allPlacesToTrans = concatMap (\(pre, _, _) -> pre) (connections n)
-  let initialMap = M.fromSet (const 0) (places n)
-  guard $ case incomingArrowsPerPlace of
-    (0, Nothing) -> True
-    _ -> let countMap = M.fromListWith (+) [(place, 1) | place <- allTransToPlaces]
-                        `M.union` initialMap
-         in all (inBounds incomingArrowsPerPlace) $ M.elems countMap
-  guard $ case outgoingArrowsPerPlace of
-    (0, Nothing) -> True
-    _ -> let countMap = M.fromListWith (+) [(place, 1) | place <- allPlacesToTrans]
-                        `M.union` initialMap
-         in all (inBounds outgoingArrowsPerPlace) $ M.elems countMap
-  guard $ case totalArrowsFromPlacesToTransitions of
-    (0, Nothing) -> True
-    _ -> inBounds totalArrowsFromPlacesToTransitions (length allPlacesToTrans)
-  guard $ case totalArrowsFromTransitionsToPlaces of
-    (0, Nothing) -> True
-    _ -> inBounds totalArrowsFromTransitionsToPlaces (length allTransToPlaces)
-  return n
-
 -- | Generate a net with limits and filtering for isolated nodes and transition behavior constraints,
 -- with pregenerated fusable connections
 netLimitsFilteredWith
@@ -239,6 +205,40 @@ netLimitsFilteredWith
     fixMaximum (low, high) = (low, fromMaybe numPlaces high)
     (vLow, vHigh) = fixMaximum (incomingArrowsPerTransition arrowDensityConstraints)
     (nLow, nHigh) = fixMaximum (outgoingArrowsPerTransition arrowDensityConstraints)
+
+-- | Apply filtering checks to a net based on arrow density constraints and transition behavior
+applyNetFiltering
+  :: (Ord s, Ord t)
+  => ArrowDensityConstraints
+  -> TransitionBehaviorConstraints
+  -> Net s t
+  -> Maybe (Net s t)
+applyNetFiltering ArrowDensityConstraints{..} transitionBehaviorConstraints n = do
+  -- Filter out nets with isolated nodes
+  guard $ not $ hasIsolatedNodes n
+  -- Filter out nets that don't satisfy transition behavior constraints
+  guard $ satisfiesTransitionBehaviorConstraints n transitionBehaviorConstraints
+  -- Filter out nets that don't satisfy arrow density constraints beyond incomingArrowsPerTransition and outgoingArrowsPerTransition
+  let allTransToPlaces = concatMap (\(_, _, post) -> post) (connections n)
+  let allPlacesToTrans = concatMap (\(pre, _, _) -> pre) (connections n)
+  let initialMap = M.fromSet (const 0) (places n)
+  guard $ case incomingArrowsPerPlace of
+    (0, Nothing) -> True
+    _ -> let countMap = M.fromListWith (+) [(place, 1) | place <- allTransToPlaces]
+                        `M.union` initialMap
+         in all (inBounds incomingArrowsPerPlace) $ M.elems countMap
+  guard $ case outgoingArrowsPerPlace of
+    (0, Nothing) -> True
+    _ -> let countMap = M.fromListWith (+) [(place, 1) | place <- allPlacesToTrans]
+                        `M.union` initialMap
+         in all (inBounds outgoingArrowsPerPlace) $ M.elems countMap
+  guard $ case totalArrowsFromPlacesToTransitions of
+    (0, Nothing) -> True
+    _ -> inBounds totalArrowsFromPlacesToTransitions (length allPlacesToTrans)
+  guard $ case totalArrowsFromTransitionsToPlaces of
+    (0, Nothing) -> True
+    _ -> inBounds totalArrowsFromTransitionsToPlaces (length allTransToPlaces)
+  return n
 
 -- | Generate a net with limits and filtering for isolated nodes and transition behavior constraints
 netLimitsFiltered
