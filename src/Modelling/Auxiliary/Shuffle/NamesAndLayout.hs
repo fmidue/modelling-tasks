70903dd1d66448d9070fc794b13b55e9a1e268e6
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2026-01-26 16:02:38 +0100

    hlint complaint

--- src/Modelling/Auxiliary/Shuffle/NamesAndLayout.hs
+++ src/Modelling/Auxiliary/Shuffle/NamesAndLayout.hs
@@ -69,4 +69,4 @@ shuffleInstanceWith
   => ShuffleInstance a
   -> g
   -> Either SomeException a
-shuffleInstanceWith x g = evalRandT (shuffleInstance x) g
+shuffleInstanceWith = evalRandT . shuffleInstance
