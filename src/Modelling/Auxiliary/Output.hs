b6b1cdc2265f0029356c070b77a1d2dbbbd02f99
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2026-02-17 16:18:04 +0100

    wording

--- src/Modelling/Auxiliary/Output.hs
+++ src/Modelling/Auxiliary/Output.hs
@@ -46,7 +46,7 @@ hoveringInformation isCollapsed = collapsed isCollapsed (translations $ do
     |]
   german [iii|
     Beim Bewegen über oder Klicken auf
-    Knoten / Kanten bzw. ihre Beschriftungen
+    Knoten / Kanten oder ihre Beschriftungen
     werden die jeweils zusammengehörenden Diagrammelemente hervorgehoben.
     |]
 
