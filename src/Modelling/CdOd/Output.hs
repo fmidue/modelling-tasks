6d090f1096437cc09f314e262021a459e714c422
nimec01 <24428341+nimec01@users.noreply.github.com>
2026-03-16 13:48:15 +0100

    remove some leftovers

--- src/Modelling/CdOd/Output.hs
+++ src/Modelling/CdOd/Output.hs
@@ -218,11 +218,10 @@ drawCd config marking mLabelLength cd@AnyClassDiagram {..} = do
         ]
   let thickenedRelationshipsWithIds = zip [0 :: Int ..] $ calculateThickAnyRelationships cd
       originalNames = mapMaybe (\(i,(_,r)) -> (i,) <$> anyRelationshipName r) thickenedRelationshipsWithIds
-      renamedThickenedRelationshipsWithIds = thickenedRelationshipsWithIds
 
       renameAnyRelationship = if isJust mLabelLength then \c -> bimap (c <$) (c <$) else const id
       renameThickenedRelationships = map (\(i,(b,r)) -> (i,(b,renameAnyRelationship (replicate (fromJust mLabelLength) 'X') r)))
-      renamedIndexedThickenedRelationships = toIndexed $ renameThickenedRelationships renamedThickenedRelationshipsWithIds
+      renamedIndexedThickenedRelationships = toIndexed $ renameThickenedRelationships thickenedRelationshipsWithIds
   let graph = mkGraph (zip [0..] theNodes) renamedIndexedThickenedRelationships
         :: Gr String (Int, (Bool, AnyRelationship String String))
   let params = nonClusteredParams {
