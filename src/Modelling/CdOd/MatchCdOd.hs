c0286215a13e07d1c9eeb34864d2f852326d36c9
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2026-03-26 10:13:57 +0100

    Fix type conversion in MatchCdOd.hs

--- src/Modelling/CdOd/MatchCdOd.hs
+++ src/Modelling/CdOd/MatchCdOd.hs
@@ -283,7 +283,7 @@ checkOdDistributionConfig maxInstances OdDistributionConfig {..}
     'objectDiagramCount' must be less than or equal to 2 * 'maxPerJustEachCd' + 'maxSharedBetweenBothCds' + 'maxNoCd'.
     |]
   | maybe False
-      ((\ mi -> maximum [maxPerJustEachCd, maxSharedBetweenBothCds, maxNoCd] > mi) . fromIntegral)
+      (\mi -> maximum [maxPerJustEachCd, maxSharedBetweenBothCds, maxNoCd] > fromIntegral mi)
       maxInstances
   = Just [iii|
     'maxPerJustEachCd', 'maxSharedBetweenBothCds' and 'maxNoCd' must be less than or equal to 'maxInstances'.
