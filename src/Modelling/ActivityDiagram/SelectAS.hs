69d24f13bacbe65311396cdfa81e2af5aea8d2b2
Ching Pei Yang <59727193+horriblename@users.noreply.github.com>
2026-05-11 08:29:20 +0200

    tweak SelectAS display (#639)
    * display SelectAS answer as index number
    
    * show choice index as text in SelectAS

--- src/Modelling/ActivityDiagram/SelectAS.hs
+++ src/Modelling/ActivityDiagram/SelectAS.hs
@@ -301,7 +301,7 @@ selectASTask showInputHelp path task = do
   paragraph $ translate $ do
     english "Consider the sequences given here:"
     german "Betrachten Sie die hier gegebenen Folgen:"
-  enumerateM (code . show) $ map (\(n,xs) -> (n, code $ show xs)) mapping
+  enumerateM (text . show) $ map (\(n,xs) -> (n, code $ show xs)) mapping
   paragraph $ do
    translate $ do
     english [i|Which of these sequences is a valid action sequence?
@@ -359,10 +359,10 @@ selectASEvaluation task n = do
         english "action sequence"
         german "Aktionsfolge"
       solMap = actionSequences task
-      (solution, validAS) = head $ M.toList $ M.map snd $ M.filter fst solMap
+      solution = head $ M.keys $ M.filter fst solMap
       solutionString =
         if showSolution task
-        then Just . (DefiniteArticle,) $ show validAS
+        then Just . (DefiniteArticle,) $ show solution
         else Nothing
   singleChoice as solutionString solution n
 
