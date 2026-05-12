c9b9f42d281d0da68e28d5da3f7386ff56c4a163
Ching Pei Yang <pei.ching@stud.uni-due.de>
2026-05-12 12:55:39 +0200

    revert ineffectful skip of check in syntax

--- src/Modelling/CdOd/NameCdError.hs
+++ src/Modelling/CdOd/NameCdError.hs
@@ -644,14 +644,10 @@ nameCdErrorSyntax inst x = do
     english "Feedback on chosen reason:"
     german "Hinweis zum gewählten Grund:"
   singleChoiceSyntax False (M.keys $ errorReasons inst) $ reason x
-  if null (dueTo x)
-    then pure ()
-    else
-      paragraph (translate $ do
-        english "Feedback on chosen relationships:"
-        german "Hinweis zu gewählten Beziehungen:"
-        )
-      *> multipleChoiceSyntax False (map fst $ relevantRelationships inst) (dueTo x)
+  paragraph $ translate $ do
+    english "Feedback on chosen relationships:"
+    german "Hinweis zu gewählten Beziehungen:"
+  multipleChoiceSyntax False (map fst $ relevantRelationships inst) (dueTo x)
   pure ()
 
 {-| Grading is done the following way:
