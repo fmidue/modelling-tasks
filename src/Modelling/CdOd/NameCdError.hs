4e9b14c0d53557837592712ab517bd13939702ba
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2026-05-14 19:02:17 +0200

    relayout as copilot suggested

--- src/Modelling/CdOd/NameCdError.hs
+++ src/Modelling/CdOd/NameCdError.hs
@@ -658,12 +658,12 @@ nameCdErrorSyntax inst x = do
     english "Feedback on chosen reason:"
     german "Hinweis zum gewählten Grund:"
   singleChoiceSyntax False (M.keys $ errorReasons inst) $ reason x
-  unless (null (dueTo x)) $
-   paragraph (translate $ do
+  unless (null (dueTo x)) $ do
+   paragraph $ translate $ do
     english "Feedback on chosen relationships:"
     german "Hinweis zu gewählten Beziehungen:"
-    )
-   *> multipleChoiceSyntax False (map fst $ relevantRelationships inst) (dueTo x)
+   multipleChoiceSyntax False (map fst $ relevantRelationships inst) (dueTo x)
+   pure ()
   pure ()
 
 {-| Grading is done the following way:
