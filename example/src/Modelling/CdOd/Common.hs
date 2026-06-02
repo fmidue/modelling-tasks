a2fc4a8f986437711f3c348203c0d8182bc798fb
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2026-06-02 22:29:29 +0200

    Update validity advice text for class diagrams

--- example/src/Modelling/CdOd/Common.hs
+++ example/src/Modelling/CdOd/Common.hs
@@ -15,8 +15,8 @@ validClassDiagramAdviceTitle = M.fromList [
 
 validClassDiagramAdviceText :: M.Map Language String
 validClassDiagramAdviceText = M.fromList [
-  (English, "A class diagram is only valid if it can actually be instantiated, i.e. if there exists at least one object diagram conforming to it. Note in particular that combinations of compositions can make a diagram non-instantiable, even though each relationship looks fine on its own."),
-  (German, "Ein Klassendiagramm ist nur dann gültig, wenn es tatsächlich instanziiert werden kann, d. h. wenn es mindestens ein dazu passendes Objektdiagramm gibt. Beachten Sie insbesondere, dass Kombinationen von Kompositionen ein Diagramm nicht-instanziierbar machen können, obwohl jede Beziehung für sich genommen in Ordnung erscheint.")
+  (English, "One reason for not considering a class diagram valid is if it cannot actually be instantiated, i.e., if there exists no object diagram conforming to it."),
+  (German, "Ein Grund dafür, ein Klassendiagramm nicht als gültig anzusehen ist, wenn es gar nicht instanziiert werden kann, d.h., wenn kein dazu passendes Objektdiagramm existiert.")
   ]
 
 validClassDiagramAdvice :: ExtraText
