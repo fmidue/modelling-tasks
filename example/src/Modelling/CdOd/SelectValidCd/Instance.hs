04164c343bfa88b37298d1872405a95e4c986d04
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2026-06-02 22:28:28 +0200

    Update validity hints for class diagrams

--- example/src/Modelling/CdOd/SelectValidCd/Instance.hs
+++ example/src/Modelling/CdOd/SelectValidCd/Instance.hs
@@ -833,8 +833,8 @@ task2024_09 = ShuffleInstance {
         (German, "Hinweis zur Gültigkeit von Klassendiagrammen")
         ])
       (listToFM [
-        (English, "A class diagram is only valid if it can actually be instantiated, i.e. if there exists at least one object diagram conforming to it. Note in particular that combinations of compositions can make a diagram non-instantiable, even though each relationship looks fine on its own."),
-        (German, "Ein Klassendiagramm ist nur dann gültig, wenn es tatsächlich instanziiert werden kann, d. h. wenn es mindestens ein dazu passendes Objektdiagramm gibt. Beachten Sie insbesondere, dass Kombinationen von Kompositionen ein Diagramm nicht-instanziierbar machen können, obwohl jede Beziehung für sich genommen in Ordnung erscheint.")
+        (English, "One reason for not considering a class diagram valid is if it cannot actually be instantiated, i.e., if there exists no object diagram conforming to it."),
+        (German, "Ein Grund dafür, ein Klassendiagramm nicht als gültig anzusehen ist, wenn es gar nicht instanziiert werden kann, d.h., wenn kein dazu passendes Objektdiagramm existiert.")
         ])
     },
   allowLayoutMangling = True,
@@ -1121,8 +1121,8 @@ task2024_53 = ShuffleInstance {
         (German, "Hinweis zur Gültigkeit von Klassendiagrammen")
         ])
       (listToFM [
-        (English, "A class diagram is only valid if it can actually be instantiated, i.e. if there exists at least one object diagram conforming to it. Note in particular that combinations of compositions can make a diagram non-instantiable, even though each relationship looks fine on its own."),
-        (German, "Ein Klassendiagramm ist nur dann gültig, wenn es tatsächlich instanziiert werden kann, d. h. wenn es mindestens ein dazu passendes Objektdiagramm gibt. Beachten Sie insbesondere, dass Kombinationen von Kompositionen ein Diagramm nicht-instanziierbar machen können, obwohl jede Beziehung für sich genommen in Ordnung erscheint.")
+        (English, "One reason for not considering a class diagram valid is if it cannot actually be instantiated, i.e., if there exists no object diagram conforming to it."),
+        (German, "Ein Grund dafür, ein Klassendiagramm nicht als gültig anzusehen ist, wenn es gar nicht instanziiert werden kann, d.h., wenn kein dazu passendes Objektdiagramm existiert.")
         ])
     },
   allowLayoutMangling = True,
