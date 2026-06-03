ecf7ad11ac5adf116c6a24bf8d473362a570e5eb
Zixin Wu <zixin.wu@uni-due.de>
2026-06-03 19:00:19 +0200

    only task2024_09 needs the validity advice

--- example/src/Modelling/CdOd/SelectValidCd/Instance.hs
+++ example/src/Modelling/CdOd/SelectValidCd/Instance.hs
@@ -408,15 +408,7 @@ task2024_06picked = ShuffleInstance {
           ])
         ]
       ],
-    addText = Collapsible True
-      (listToFM [
-        (English, "Hint on the validity of class diagrams"),
-        (German, "Hinweis zur Gültigkeit von Klassendiagrammen")
-        ])
-      (listToFM [
-        (English, "One reason for not considering a class diagram valid is if it cannot actually be instantiated, i.e., if there exists no object diagram conforming to it."),
-        (German, "Ein Grund dafür, ein Klassendiagramm nicht als gültig anzusehen ist, wenn es gar nicht instanziiert werden kann, d.h., wenn kein dazu passendes Objektdiagramm existiert.")
-        ])
+    addText = NoExtraText
     },
   allowLayoutMangling = True,
   shuffleNames = True,
@@ -1123,15 +1115,7 @@ task2024_53 = ShuffleInstance {
           ])
         ]
       ],
-    addText = Collapsible True
-      (listToFM [
-        (English, "Hint on the validity of class diagrams"),
-        (German, "Hinweis zur Gültigkeit von Klassendiagrammen")
-        ])
-      (listToFM [
-        (English, "One reason for not considering a class diagram valid is if it cannot actually be instantiated, i.e., if there exists no object diagram conforming to it."),
-        (German, "Ein Grund dafür, ein Klassendiagramm nicht als gültig anzusehen ist, wenn es gar nicht instanziiert werden kann, d.h., wenn kein dazu passendes Objektdiagramm existiert.")
-        ])
+    addText = NoExtraText
     },
   allowLayoutMangling = True,
   shuffleNames = False,
