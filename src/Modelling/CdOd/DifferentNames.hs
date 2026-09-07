4fbaa56d3d849f9120769688d59f1f17762a790f
Zixin Wu <zixin.wu@uni-due.de>
2026-09-07 13:28:02 +0200

    use suggested SolutionDisplay names

--- src/Modelling/CdOd/DifferentNames.hs
+++ src/Modelling/CdOd/DifferentNames.hs
@@ -213,9 +213,9 @@ data ShufflingOption a =
 data SolutionDisplay
   = ShowNothing
   | ShowMapping
-  | ShowMappingAndReprintCD
-  | ShowMappingAndReprintOD
-  | ShowMappingAndReprintCDAndOD
+  | ShowMappingAndRelabelledCd
+  | ShowMappingAndRelabelledOd
+  | ShowMappingAndCdAndRelabelledOd
   deriving (Eq, Generic, Hashable, Read, Reader, Show, ToDoc)
 
 data DifferentNamesInstance = DifferentNamesInstance {
@@ -622,7 +622,7 @@ differentNamesEvaluation path task cs = do
     case showSolution task of
       ShowNothing -> pure ()
       ShowMapping -> pure ()
-      ShowMappingAndReprintCD -> do
+      ShowMappingAndRelabelledCd -> do
         paragraph $ translate $ do
           english "Consider the correctly labelled class diagram:"
           german "Betrachten Sie das korrekt beschriftete Klassendiagramm:"
@@ -631,7 +631,7 @@ differentNamesEvaluation path task cs = do
           cacheCd (cdDrawSettings task) mempty mLabelLength (fromClassDiagram relabelledCd) path)
 
         pure ()
-      ShowMappingAndReprintOD -> do
+      ShowMappingAndRelabelledOd -> do
         paragraph $ translate $ do
           english "Consider the correctly labelled object diagram:"
           german "Betrachten Sie das korrekt beschriftete Objektdiagramm:"
@@ -640,7 +640,7 @@ differentNamesEvaluation path task cs = do
           cacheOd relabelledOd mLabelLength Forward True path)
 
         pure ()
-      ShowMappingAndReprintCDAndOD -> do
+      ShowMappingAndCdAndRelabelledOd -> do
         paragraph $ translate $ do
           english "Here is the reference class diagram:"
           german "Hier ist das Referenz-Klassendiagramm:"
